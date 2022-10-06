a4326b4b688a7ef364316f7cce6f4886a8780098
Mykel Alvis <mykel.alvis@gmail.com>
2022-10-06 08:05:57 -0500

    Add dataobjects

---
+++ ibcore-vertx-dataobjects/src/main/java/org/infrastructurebuilder/util/vertx/dataobjects/TCache.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright © 2019 admin (admin@infrastructurebuilder.org)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.infrastructurebuilder.util.vertx.dataobjects;
+
+import static io.vertx.core.Future.succeededFuture;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import io.vertx.core.Future;
+
+public interface TCache<T> {
+
+  void start();
+
+  void stop();
+
+  boolean isRunning();
+
+  Future<Map<String, T>> getAllAsMap();
+
+  Future<List<T>> getAllOfType(String type);
+
+  Future<Map<String, List<T>>> getAllAsTypeMap();
+
+  Future<T> get(String id);
+
+  default Future<List<T>> getAll() {
+    return getAllAsMap().compose(m -> succeededFuture(new ArrayList<>(m.values())));
+  }
+
+
+  /**
+   * Add an T. Will not add an T with a duplicate ID!
+   *
+   * @param i T to add
+   * @return true if added, false if T exists
+   */
+  Future<Boolean> put(T i);
+
+  /**
+   * Marks an T as deleted
+   *
+   * @param i T to mark as deleted
+   * @return
+   */
+  Future<Boolean> remove(T i);
+
+  Future<Boolean> update(T i);
+
+  Future<List<T>> getAllRemoved();
+
+  Future<Void> gc(boolean removeDeleted);
+
+  Future<Boolean> setTFactoryReference(TFactory<T> factoryImpl);
+
+  //
+  TFactory<T> getFactory();
+
+}
---
+++ ibcore-vertx-dataobjects/src/main/java/org/infrastructurebuilder/util/vertx/dataobjects/Tag.java
@@ -0,0 +1,117 @@
+/*
+ * Copyright © 2019 admin (admin@infrastructurebuilder.org)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.infrastructurebuilder.util.vertx.dataobjects;
+
+import static java.util.Objects.requireNonNull;
+import static java.util.Optional.empty;
+import static java.util.Optional.ofNullable;
+
+import java.util.Map.Entry;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.function.Function;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Tag {
+
+  private final static Logger log = LoggerFactory.getLogger(Tag.class);
+  private final String tag;
+  private final String value;
+  private final Integer intValue;
+  private final Double doubleValue;
+
+  public final static Function<String, Integer> strToInt = (str) -> {
+    try {
+      return Integer.valueOf(str);
+    } catch (Throwable t) {
+      return null;
+    }
+  };
+
+  public final static Function<String, Double> strToDouble = (str) -> {
+    try {
+      return Double.valueOf(str);
+    } catch (Throwable t) {
+      return null;
+    }
+  };
+
+  public Tag(String tag) {
+    this(tag, empty());
+  }
+
+  public Tag(String tag, String value) {
+    this(tag, ofNullable(value));
+  }
+
+  private Tag(String tag, Optional<String> value) {
+    this.tag = requireNonNull(tag).trim();
+    this.value = requireNonNull(value).map(String::trim).orElse(null);
+    this.intValue = strToInt.apply(this.value);
+    this.doubleValue = strToDouble.apply(this.value);
+  }
+
+  public Tag(Entry<String, Object> e) {
+    this(e.getKey(), Optional.ofNullable(e.getValue()).map(Object::toString).orElseGet(() -> null));
+  }
+
+  public String getTag() {
+    return tag;
+  }
+
+  public Optional<String> getValue() {
+    return ofNullable(this.value);
+  }
+
+  public Optional<Integer> getIntValue() {
+    return ofNullable(intValue);
+  }
+
+  public Optional<Double> getDoubleValue() {
+    return ofNullable(doubleValue);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(tag);
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    Tag other = (Tag) obj;
+    return Objects.equals(tag, other.tag);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder(this.tag);
+    getDoubleValue().ifPresentOrElse(dv -> sb.append("=").append(dv),
+
+        () -> getIntValue().ifPresentOrElse(iv -> sb.append("=").append(iv),
+
+            () -> getValue().ifPresent(v -> sb.append("=").append(v))));
+    return sb.toString();
+  }
+
+}
